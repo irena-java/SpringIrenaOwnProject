61869a7334a3653966cfc21bfd5a0b292343916e
Irena Vakulenko <ivvvv@ukr.net>
2021-09-14 17:52:15 +0300

    SPRING DATA & REST (homeworks 15,20)

--- src/main/java/com/datascience/shop/ApplicationStarter.java
+++ src/main/java/com/datascience/shop/ApplicationStarter.java
@@ -1,16 +1,22 @@
 package com.datascience.shop;
 
+import com.datascience.shop.entity.User;
+import com.datascience.shop.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class ApplicationStarter {
     public static void main(String[] args) {
     final ConfigurableApplicationContext ctx = SpringApplication.run(ApplicationStarter.class);
         System.out.println("БИны в контексте - "+ Arrays.toString(ctx.getBeanDefinitionNames()));
+        final UserRepository bean=ctx.getBean(UserRepository.class);
+        List<User> users= bean.findAll();
+        users.forEach(System.out::println);
     }
 }
 
